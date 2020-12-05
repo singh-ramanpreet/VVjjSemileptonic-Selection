d7eeeb0bb8ccee6c83f5fdb0e6bac4edf1e2dd16
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2020-12-04 22:42:29 -0600

    add tests

---
+++ Selection/test/make_tests.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+a=$(eos root://cmseos.fnal.gov/ find -name "*.root" /eos/uscms/store/user/lnujj/VVjj_aQGC/nanoAOD_skim/Run2016_v7_Dec2020/)
+readarray -t mc < <(grep -vE "Run201[6-8][A-Z]" <<<"$a");
+declare -p mc &> /dev/null
+
+readarray -t data < <(grep -E "Run201[6-8][A-Z]" <<<"$a");
+declare -p data &> /dev/null
+
+b=$(eos root://cmseos.fnal.gov/ find -name "*.root" /eos/uscms/store/user/lnujj/VVjj_aQGC/nanoAOD_skim/Run2016_v7_custom_Dec2020/)
+readarray -t mc_2 < <(grep -vE "Run201[6-8][A-Z]" <<<"$b");
+declare -p mc_2 &> /dev/null
+
+mc+=(${mc_2[@]})
+
+echo ${mc[0]} > test_2016_mc.txt
+echo ${mc[1]} >> test_2016_mc.txt
+echo ${data[0]} > test_2016_data.txt
+
+
+a=$(eos root://cmseos.fnal.gov/ find -name "*.root" /eos/uscms/store/user/lnujj/VVjj_aQGC/nanoAOD_skim/Run2017_v7_Dec2020/)
+readarray -t mc < <(grep -vE "Run201[6-8][A-Z]" <<<"$a");
+declare -p mc &> /dev/null
+
+readarray -t data < <(grep -E "Run201[6-8][A-Z]" <<<"$a");
+declare -p data &> /dev/null
+
+b=$(eos root://cmseos.fnal.gov/ find -name "*.root" /eos/uscms/store/user/lnujj/VVjj_aQGC/nanoAOD_skim/Run2017_v7_custom_Dec2020/)
+readarray -t mc_2 < <(grep -vE "Run201[6-8][A-Z]" <<<"$b");
+declare -p mc_2 &> /dev/null
+
+mc+=(${mc_2[@]})
+
+echo ${mc[0]} > test_2017_mc.txt
+echo ${mc[1]} >> test_2017_mc.txt
+echo ${data[0]} > test_2017_data.txt
+
+
+a=$(eos root://cmseos.fnal.gov/ find -name "*.root" /eos/uscms/store/user/lnujj/VVjj_aQGC/nanoAOD_skim/Run2018_v7_Dec2020/)
+readarray -t mc < <(grep -vE "Run201[6-8][A-Z]" <<<"$a");
+declare -p mc &> /dev/null
+
+readarray -t data < <(grep -E "Run201[6-8][A-Z]" <<<"$a");
+declare -p data &> /dev/null
+
+b=$(eos root://cmseos.fnal.gov/ find -name "*.root" /eos/uscms/store/user/lnujj/VVjj_aQGC/nanoAOD_skim/Run2018_v7_custom_Dec2020/)
+readarray -t mc_2 < <(grep -vE "Run201[6-8][A-Z]" <<<"$b");
+declare -p mc_2 &> /dev/null
+
+mc+=(${mc_2[@]})
+
+echo ${mc[0]} > test_2018_mc.txt
+echo ${mc[1]} >> test_2018_mc.txt
+echo ${data[0]} > test_2018_data.txt
---
+++ Selection/test/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Args
+recompile=${1:-true}
+
+if $recompile
+then
+  (cd $CMSSW_BASE/src/WVJJAna/Selection; scram b)
+fi
+
+for txtfile in $(ls *.txt)
+do
+  testfile=$(head -n 1 ${txtfile})
+  isMC=$(python -c "import ROOT;f=ROOT.TFile.Open('root://cmseos.fnal.gov/$testfile'); t=f.Get('Runs');print(int(t.GetBranchStatus('genEventSumw')))")
+  year=$(grep -Eo "201[6-8]" ${txtfile})
+  txtpath=$(readlink -f ${txtfile})
+  output=${txtpath%.txt}.root
+
+  echo "Testing ${txtfile}, this is MC=$isMC, year=$year, with following command"
+  echo Selection $txtpath $output $isMC $year 7
+  (cd $CMSSW_BASE/src/WVJJAna/Selection; Selection $txtpath $output $isMC $year 7;)
+done
